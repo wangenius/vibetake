96bb9ad4244abb50db682cde48ae139cdb002e11
wangenius <wangenius@qq.com>
2025-09-11 16:18:51 +0800

    添加了fumadocs

---
+++ .source/index.ts
@@ -0,0 +1,5 @@
+// @ts-nocheck -- skip type checking
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1757576965068"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/Users/wangenius/Documents/github/template/content/docs/index.mdx"}, data: docs_0 }], [])
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import { createMDX } from "fumadocs-mdx/next";
 import type { NextConfig } from "next";
+
+const withMDX = createMDX({
+  configPath: "source.config.ts",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
